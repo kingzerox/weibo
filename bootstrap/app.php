27248ba9abc0b92ea89c60eacdad5e5e06ae4c92
kingzerox <kingzerox@hotmail.com>
2019-03-12 08:23:58 +0000

    中文的错误提示信息

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -51,5 +51,6 @@ $app->singleton(
 | from the actual running of the application and sending responses.
 |
 */
+$app->register(Overtrue\LaravelLang\TranslationServiceProvider::class);
 
 return $app;
